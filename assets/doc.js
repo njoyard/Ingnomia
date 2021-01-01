ebf98c54ac0a5c0581ee3234e8788edacbe3fcc4
Nicolas Joyard <nicolas@njoyard.fr>
2021-01-01 22:42:46 +0100

    update

--- assets/doc.js
+++ assets/doc.js
@@ -1,4 +1,4 @@
-;(function () {
+function setupSearch() {
   let input = document.querySelector('input[type="search"]')
   let queryDisplay = document.querySelector('.search-query')
   let results = document.querySelector('section.search-results')
@@ -53,4 +53,49 @@
   input.value = ''
   input.addEventListener('keyup', search)
   input.addEventListener('change', search)
+}
+
+function setupCollapse() {
+  for (let header of document.querySelectorAll('[data-collapse]')) {
+    let group = header.getAttribute('data-collapse')
+    let trigger = document.createElement('a')
+
+    trigger.className = 'collapse-trigger'
+    trigger.href = '#'
+    trigger.setAttribute('data-collapsed', 'false')
+    trigger.textContent = '(collapse)'
+
+    trigger.addEventListener('click', (e) => {
+      e.preventDefault()
+
+      let collapsed = trigger.getAttribute('data-collapsed')
+      if (collapsed === 'true') {
+        trigger.setAttribute('data-collapsed', 'false')
+        trigger.textContent = '(collapse)'
+
+        for (let elem of document.querySelectorAll(
+          `[data-collapse-group="${group}"]`
+        )) {
+          elem.style.display = 'table-row'
+        }
+      } else {
+        trigger.setAttribute('data-collapsed', 'true')
+        trigger.textContent = '(expand)'
+
+        for (let elem of document.querySelectorAll(
+          `[data-collapse-group="${group}"]`
+        )) {
+          elem.style.display = 'none'
+        }
+      }
+    })
+
+    header.appendChild(trigger)
+    trigger.click()
+  }
+}
+
+;(function () {
+  setupSearch()
+  setupCollapse()
 })()
