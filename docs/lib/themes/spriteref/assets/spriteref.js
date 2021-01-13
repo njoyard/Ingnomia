d53be3347467c9d0f71452e64ce067969e634c72
Nicolas Joyard <nicolas@njoyard.fr>
2021-01-13 22:17:42 +0100

    wip

--- docs/lib/themes/spriteref/assets/spriteref.js
+++ docs/lib/themes/spriteref/assets/spriteref.js
@@ -1,43 +1,395 @@
-;(function () {
-  let basesprites = document.querySelectorAll('.basesprite')
-  let info = document.querySelector('.info')
+const TILEW = 32
+const TILEH = 36
 
-  function showInfo(ts, rect) {
-    info.innerHTML = ''
+const state = {}
 
-    let title = document.createElement('div')
-    title.className = 'title'
-    title.textContent = `${ts} @${rect}`
+function elem(name, props = {}) {
+  let e = document.createElement(name)
 
-    info.appendChild(title)
+  if (props.class) {
+    e.className = props.class
+  }
 
-    for (let match of [
-      ...document.querySelectorAll(
-        `[data-tilesheet="${ts}"][data-rect="${rect}"]`
-      )
-    ]) {
-      let id = document.createElement('div')
-      id.className = 'id'
-      id.textContent = match.getAttribute('data-id')
+  if (props.style) {
+    for (let k in props.style) {
+      e.style[k] = props.style[k]
+    }
+  }
 
-      info.appendChild(id)
+  if (props.data) {
+    for (let k in props.data) {
+      e.dataset[k] = props.data[k]
+    }
+  }
+
+  if (props.text) {
+    e.textContent = props.text
+  }
+
+  return e
+}
+
+function setupUnusedSpots(ts, file, width, height) {
+  let container = ts.querySelector('.tilesheet-container')
+
+  for (let x = 0; x < width; x += TILEW) {
+    for (let y = 0; y < height; y += TILEH) {
+      let rect = `${x} ${y} ${TILEW} ${TILEH}`
+
+      if (
+        document.querySelector(
+          `.basesprite[data-rect="${rect}"][data-tilesheet="${file}"]`
+        )
+      ) {
+        continue
+      }
+
+      let unused = elem('div', {
+        class: 'unused',
+        data: { rect, tilesheet: file },
+        style: { left: `${x}px`, top: `${y}px` }
+      })
+
+      setupUnusedEvents(unused)
+      container.appendChild(unused)
+    }
+  }
+}
+
+function loadTilesheet(ts) {
+  let file = ts.dataset.tilesheet
+
+  return new Promise((resolve) => {
+    let img = new Image()
+    img.src = `assets/${file}`
+    img.addEventListener('load', (e) => {
+      let canvas = ts.querySelector('canvas')
+      let ctx = canvas.getContext('2d')
+      canvas.width = img.width
+      canvas.height = img.height
+      ctx.drawImage(img, 0, 0)
+
+      setupUnusedSpots(ts, file, img.width, img.height)
+      resolve()
+    })
+  })
+}
+
+function showCode(code) {
+  state.code.querySelector('code').textContent = code
+  state.code.style.display = 'block'
+}
+
+async function copyCode() {
+  await navigator.clipboard.writeText(
+    state.code.querySelector('code').textContent
+  )
+}
+
+function hideCode() {
+  state.code.style.display = 'none'
+}
+
+function showInfo(ts, rect) {
+  state.info.innerHTML = ''
+
+  let title = elem('div', { class: 'title', text: `${ts} @${rect}` })
+  state.info.appendChild(title)
+
+  let hasBase = false
+
+  for (let match of [
+    ...document.querySelectorAll(
+      `.basesprite[data-tilesheet="${ts}"][data-rect="${rect}"]`
+    )
+  ]) {
+    let uses = match.dataset.uses
+    uses = uses == '' ? [] : uses.split(',')
+
+    let id = elem('div', { class: 'id', text: match.dataset.id })
+    state.info.appendChild(id)
+
+    for (let use of uses) {
+      let u = elem('div', { class: 'use', text: use })
+      state.info.appendChild(u)
     }
 
-    info.style.display = 'block'
+    hasBase = true
   }
 
-  function hideInfo() {
-    info.style.display = 'none'
-  }
-
-  for (let basesprite of basesprites) {
-    basesprite.addEventListener('mouseenter', (e) => {
-      showInfo(
-        e.target.getAttribute('data-tilesheet'),
-        e.target.getAttribute('data-rect')
-      )
+  if (hasBase) {
+    let a = elem('div', {
+      class: 'action',
+      text: 'Click to select'
     })
 
-    basesprite.addEventListener('mouseout', hideInfo)
+    state.info.appendChild(a)
   }
+
+  for (let match of [
+    ...document.querySelectorAll(
+      `.unused[data-tilesheet="${ts}"][data-rect="${rect}"]`
+    )
+  ]) {
+    let id = elem('div', { class: 'id', text: '<Unallocated>' })
+    state.info.appendChild(id)
+
+    if (state.selected) {
+      let bsid = state.selected.dataset.id
+      let a = elem('div', {
+        class: 'action',
+        text: `Click to move ${bsid} here`
+      })
+
+      state.info.appendChild(a)
+    }
+  }
+
+  state.info.style.display = 'block'
+}
+
+function hideInfo() {
+  state.info.style.display = 'none'
+}
+
+function selectTilesheet(delta) {
+  let cur = state.tilesheet
+  let index = state.tilesheets.indexOf(cur)
+  let next =
+    state.tilesheets[
+      (index + delta + state.tilesheets.length) % state.tilesheets.length
+    ]
+
+  cur.style.display = 'none'
+  next.style.display = 'block'
+
+  state.tilesheet = next
+}
+
+function toggleOverlays() {
+  document.body.classList.toggle('overlays')
+}
+
+function dumpBaseSprites() {
+  let lines = []
+
+  // Keep original DB ordering
+  let sorted = [...document.querySelectorAll('.basesprite')].sort(
+    (a, b) => Number(a.dataset.index) - Number(b.dataset.index)
+  )
+
+  for (let bs of sorted) {
+    let id = bs.dataset.id
+    let rect = bs.dataset.rect
+    let ts = bs.dataset.tilesheet
+
+    lines.push(
+      `INSERT INTO "BaseSprites" ("ID","SourceRectangle","Tilesheet") VALUES ('${id}','${rect}','${ts}');`
+    )
+  }
+
+  showCode(lines.join('\n'))
+}
+
+function dumpImageOps() {
+  showCode(state.imageOps.join('\n'))
+}
+
+function selectBaseSprite(bs) {
+  if (state.selected) {
+    state.selected.classList.remove('selected')
+  }
+
+  state.selected = bs
+  bs.classList.add('selected')
+}
+
+function cancelSelection() {
+  if (state.selected) {
+    state.selected.classList.remove('selected')
+    state.selected = null
+  }
+}
+
+function moveSelectedTo(unused) {
+  if (state.selected) {
+    let bs = state.selected
+
+    let from_parent = bs.parentNode
+    let from_ts = bs.dataset.tilesheet
+    let from_rect = bs.dataset.rect
+    let [from_left, from_top] = from_rect.split(' ')
+    let from_canvas = from_parent.querySelector('canvas')
+    let from_ctx = from_canvas.getContext('2d')
+    let from_data = from_ctx.getImageData(from_left, from_top, TILEW, TILEH)
+
+    let to_parent = unused.parentNode
+    let to_ts = unused.dataset.tilesheet
+    let to_rect = unused.dataset.rect
+    let [to_left, to_top] = to_rect.split(' ')
+    let to_canvas = to_parent.querySelector('canvas')
+    let to_ctx =
+      to_canvas === from_canvas ? from_ctx : to_canvas.getContext('2d')
+
+    // Copy pixels
+    to_ctx.putImageData(from_data, to_left, to_top)
+    to_parent.parentNode.dataset.dirty = 'true'
+
+    // Update and move basesprite element
+    bs.dataset.tilesheet = to_ts
+    bs.dataset.rect = to_rect
+    bs.style.left = `${to_left}px`
+    bs.style.top = `${to_top}px`
+    bs.classList.add('moved')
+    from_parent.removeChild(bs)
+    to_parent.appendChild(bs)
+
+    // Remove or update&move unused element
+    to_parent.removeChild(unused)
+    if (
+      !document.querySelector(
+        `.basesprite[data-rect="${from_rect}"][data-tilesheet="${from_ts}"]`
+      )
+    ) {
+      // No more basesprite at from location, move unused there
+      unused.dataset.tilesheet = from_ts
+      unused.dataset.rect = from_rect
+      unused.style.left = `${from_left}px`
+      unused.style.top = `${from_top}px`
+      from_parent.appendChild(unused)
+    }
+
+    state.imageOps.push(
+      `convert ${to_ts} \\( ${from_ts} -crop ${TILEW}x${TILEH}+${from_left}+${from_top} \\) -geometry +${to_left}+${to_top} -compose copy -composite ${to_ts}`
+    )
+
+    cancelSelection()
+  }
+}
+
+function clearSprite() {
+  if (state.hover) {
+    if (state.hover.classList.contains('unused')) {
+      let ts = state.tilesheet.dataset.tilesheet
+      let [left, top] = state.hover.dataset.rect.split(' ')
+      let canvas = state.tilesheet.querySelector('canvas')
+      let ctx = canvas.getContext('2d')
+
+      ctx.clearRect(left, top, TILEW, TILEH)
+      state.tilesheet.dataset.dirty = 'true'
+
+      state.imageOps.push(
+        `convert ${ts} \\( -size ${TILEW}x${TILEH} xc:none \\) -alpha set -geometry +${left}+${top} -compose copy -composite ${ts}`
+      )
+
+      state.hover.classList.add('cleared')
+    }
+  }
+}
+
+const KEYS = [
+  // Tab => switch tilesheets
+  { keyCode: 9, shift: true, handler: () => selectTilesheet(-1) },
+  {
+    keyCode: 9,
+    shift: false,
+    handler: () => selectTilesheet(1),
+    button: '#tilesheet'
+  },
+
+  // Esc => cancel selection
+  { keyCode: 27, shift: false, handler: cancelSelection, button: '#unselect' },
+
+  // O => toggle color overlays
+  { keyCode: 79, shift: false, handler: toggleOverlays, button: '#overlays' },
+
+  // D => dump basesprites
+  { keyCode: 68, shift: false, handler: dumpBaseSprites, button: '#dump' },
+
+  // I => dump image ops
+  { keyCode: 73, shift: false, handler: dumpImageOps, button: '#imgops' },
+
+  // C => clear sprite under cursor
+  { keyCode: 67, shift: false, handler: clearSprite, button: '#clear' }
+]
+
+function setupBasespriteEvents(basesprite) {
+  basesprite.addEventListener('mouseenter', (e) => {
+    state.hover = e.target
+    showInfo(e.target.dataset.tilesheet, e.target.dataset.rect)
+  })
+  basesprite.addEventListener('mouseout', () => {
+    state.hover = null
+    hideInfo()
+  })
+  basesprite.addEventListener('click', (e) => selectBaseSprite(e.target))
+}
+
+function setupUnusedEvents(unused) {
+  unused.addEventListener('mouseenter', (e) => {
+    state.hover = e.target
+    showInfo(e.target.dataset.tilesheet, e.target.dataset.rect)
+  })
+  unused.addEventListener('mouseout', () => {
+    state.hover = null
+    hideInfo()
+  })
+  unused.addEventListener('click', (e) => moveSelectedTo(e.target))
+}
+
+function setupUI() {
+  let cur = state.tilesheets[0]
+  cur.style.display = 'block'
+
+  state.imageOps = []
+  state.tilesheet = cur
+  state.code = document.querySelector('.code')
+
+  state.code.querySelector('#hide').addEventListener('click', hideCode)
+  state.code.querySelector('#copy').addEventListener('click', copyCode)
+
+  for (let { button, handler } of KEYS) {
+    if (button) {
+      document
+        .querySelector(`.controls ${button}`)
+        .addEventListener('click', handler)
+    }
+  }
+
+  document.addEventListener('keydown', (e) => {
+    let match = KEYS.find(
+      (k) => k.keyCode === e.keyCode && k.shift === e.shiftKey
+    )
+
+    if (match) {
+      e.preventDefault()
+      match.handler()
+    }
+  })
+
+  document.querySelector('.controls').style.display = 'block'
+  document.querySelector('.loading').style.display = 'none'
+
+  for (let basesprite of [...document.querySelectorAll('.basesprite')]) {
+    setupBasespriteEvents(basesprite)
+  }
+
+  window.addEventListener('beforeunload', (e) => {
+    let dirty = [...document.querySelectorAll('[data-dirty="true"]')].map(
+      (t) => t.dataset.tilesheet
+    )
+
+    if (dirty.length) {
+      e.preventDefault()
+      e.returnValue = ''
+    }
+  })
+}
+
+;(async function () {
+  state.info = document.querySelector('.info')
+  state.tilesheets = [...document.querySelectorAll('.tilesheet')]
+
+  await Promise.all(state.tilesheets.map((ts) => loadTilesheet(ts)))
+  setupUI()
 })()
--- docs/lib/themes/spriteref/theme.py
+++ docs/lib/themes/spriteref/theme.py
@@ -3,7 +3,7 @@ import shutil
 from jinja2 import Environment, FileSystemLoader
 
 from ...db import db
-from ...util import DOCDIR, sprite_rect
+from ...util import DOCDIR, sprite_rect, empty
 
 THEME_PATH = os.path.dirname(__file__)
 ASSET_PATH = os.path.join(THEME_PATH, "assets")
@@ -31,18 +31,147 @@ class SpriteRefTheme:
             for (rect, tilesheet) in [db.basesprite(id)]
         ]
 
+        for id in set([bs["id"] for bs in basesprites]):
+            matches = [bs for bs in basesprites if bs["id"] == id]
+            if len(matches) > 1:
+                print(f"Duplicate BaseSprite: {id} ({len(matches)} copies)")
+
+        uses = {bs["id"]: [] for bs in basesprites}
+
+        for id in db.sprites():
+            (offset, base, tint) = db.sprite(id)
+
+            sbm = list(db.sprite_by_material(id))
+            if len(sbm):
+                for (base, mat, sprite, effect) in sbm:
+                    if not empty(base):
+                        uses[base].append(f"{id} for material {mat}")
+                continue
+
+            sbmt = list(db.sprite_by_material_type(id))
+            if len(sbmt):
+                for (base, mat, sprite) in sbmt:
+                    if not empty(base):
+                        uses[base].append(f"{id} for material {mat} (via type)")
+                continue
+
+            sc = list(db.sprite_combine(id))
+            if len(sc):
+                for (base, off, sprite, tint) in sc:
+                    if not empty(base):
+                        uses[base].append(f"{id} as combined part")
+                continue
+
+            sf = list(db.sprite_frames(id))
+            if len(sf):
+                for (base,) in sf:
+                    if not empty(sf):
+                        uses[base].append(f"{id} as animation frame")
+                continue
+
+            sr = list(db.sprite_random(id))
+            if len(sr):
+                for (base, sprite) in sr:
+                    if not empty(base):
+                        uses[base].append(f"{id} as random variation")
+                continue
+
+            sro = list(db.sprite_rotations(id))
+            if len(sro):
+                for (base, sprite, rot, effect) in sro:
+                    if not empty(base):
+                        try:
+                            uses[base].append(f"{id} as rotation {rot} {effect}")
+                        except KeyError:
+                            print(f"Invalid base: {base} in sprite rotation {id}")
+                continue
+
+            ss = list(db.sprite_seasons(id))
+            if len(ss):
+                for (base, season) in ss:
+                    if not empty(base):
+                        uses[base].append(f"{id} for season {season}")
+                    else:
+                        ssr = list(db.sprite_seasons_rotations(id, season))
+                        for (base, rot) in ssr:
+                            uses[base].append(f"{id} for season {season}, rotation {rot}")
+                continue
+
+            if not empty(base):
+                try:
+                    uses[base].append(f"{id}")
+                except KeyError:
+                    print(f"Invalid base: {base} in sprite {id}")
+            elif id in uses:
+                uses[id].append(f"{id}")
+
         tilesheets = sorted(
             [
-                {"filename": tilesheet, "basesprites": [bs for bs in basesprites if bs["tilesheet"] == tilesheet]}
+                {
+                    "filename": tilesheet,
+                    "basesprites": [
+                        {"index": basesprites.index(bs), **bs} for bs in basesprites if bs["tilesheet"] == tilesheet
+                    ],
+                }
                 for tilesheet in set([b["tilesheet"] for b in basesprites])
             ],
             key=lambda ts: ts["filename"],
         )
 
         with open(os.path.join(output, "index.html"), mode="w") as f:
-            f.write(self.env.get_template("index.html.j2").render(tilesheets=tilesheets))
+            f.write(self.env.get_template("index.html.j2").render(tilesheets=tilesheets, uses=uses))
 
         for asset in self.assets:
             shutil.copy(os.path.join(ASSET_PATH, asset), os.path.join(output, "assets", asset))
         for asset in map(lambda ts: ts["filename"], tilesheets):
             shutil.copy(os.path.join(CONTENT_PATH, "tilesheet", asset), os.path.join(output, "assets", asset))
+
+    # def sprite(self, id):
+    #     # Keep last row only
+    #     row = None
+    #     for r in self.conn.execute("SELECT offset, basesprite, tint FROM Sprites WHERE id = ?", (id,)):
+    #         row = r
+    #     return row
+
+    # def sprite_by_material(self, id):
+    #     return self.conn.execute(
+    #         "SELECT basesprite, materialid, sprite, effect FROM Sprites_ByMaterials WHERE id = ?",
+    #         (id,),
+    #     )
+
+    # def sprite_by_material_type(self, id):
+    #     return self.conn.execute(
+    #         """SELECT bmt.basesprite, m.id, bmt.sprite
+    #            FROM Sprites_ByMaterialTypes bmt
+    #            JOIN Materials m ON m.type = bmt.materialtype
+    #            WHERE bmt.id = ?""",
+    #         (id,),
+    #     )
+
+    # def sprite_combine(self, id):
+    #     return self.conn.execute(
+    #         "SELECT basesprite, offset, sprite, tint FROM Sprites_Combine WHERE id = ?",
+    #         (id,),
+    #     )
+
+    # def sprite_frames(self, id):
+    #     return self.conn.execute("SELECT DISTINCT basesprite FROM Sprites_Frames WHERE id = ?", (id,))
+
+    # def sprite_random(self, id):
+    #     return self.conn.execute("SELECT basesprite, sprite FROM Sprites_Random WHERE id = ?", (id,))
+
+    # def sprite_rotations(self, id):
+    #     return self.conn.execute(
+    #         "SELECT basesprite, sprite, rotation, effect FROM Sprites_Rotations WHERE id = ?", (id,)
+    #     )
+
+    # def sprite_seasons(self, id):
+    #     return self.conn.execute("SELECT basesprite, season FROM Sprites_Seasons WHERE id = ?", (id,))
+
+    # def sprite_seasons_rotations(self, id, season):
+    #     return self.conn.execute(
+    #         "SELECT basesprite, rotation FROM Sprites_Seasons_Rotations WHERE id = ?", (f"{id}{season}",)
+    #     )
+
+    # def sprites(self):
+    #     return [id for (id,) in self.conn.execute("SELECT id FROM Sprites")]
